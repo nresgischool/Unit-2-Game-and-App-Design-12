5e5c4c496a7c84c22dc089734441ab34137e45fb
aaaaaaaaaaaaa <74013790+nresgischool@users.noreply.github.com>
2022-03-04 00:07:57 -0800

    start on working on shooting projectiles

--- Unit2/Assets/Scripts/PlayerFallWarp.cs
+++ Unit2/Assets/Scripts/PlayerFallWarp.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerFallWarp : MonoBehaviour
 {
     public GameObject warpPoint;
+    public GameObject warpPoint2;
     public float teleportFloorLevel;
+    public bool spawnNew = false;
 
     // Update is called once per frame
     void Update()
@@ -14,12 +16,28 @@ public class PlayerFallWarp : MonoBehaviour
         {
             CharacterController controller = GetComponent<CharacterController>();
 
-            if (controller != null)
+            if (controller != null && spawnNew == true)
+            {
+                controller.enabled = false;
+                controller.transform.position = warpPoint2.transform.position;
+                controller.enabled = true;
+            }
+            else if (controller != null)
             {
                 controller.enabled = false;
                 controller.transform.position = warpPoint.transform.position;
                 controller.enabled = true;
             }
+
         }
+    }
+    // Detect collision with the SpawnTrigger tag to set a new spawnpoint
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "SpawnTrigger")
+        {
+            Debug.Log("nwe spawn");
+            spawnNew = true;
+        }    
     }
 }
